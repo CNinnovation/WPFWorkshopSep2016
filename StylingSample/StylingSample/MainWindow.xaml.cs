e961a4480546ae8f094a27b9250fbf11959d10d3
Christian Nagel <christian@christiannagel.com>
2016-09-06 10:52:56 +0200

    styles and templates

---
+++ StylingSample/StylingSample/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StylingSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+            var newBrush = new RadialGradientBrush();
+            newBrush.GradientStops.Add(new GradientStop(Colors.Black, 0));
+            newBrush.GradientStops.Add(new GradientStop(Colors.Yellow, 1));
+            this.Resources["redBrush"] = newBrush;
+
+        }
+    }
+}
