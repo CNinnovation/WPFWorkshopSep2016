f8262c831a3ebbf27ed3faa52c2cc51853f9d252
Christian Nagel <christian@christiannagel.com>
2016-09-08 13:03:24 +0200

    TreeView sample

---
+++ TreeViewSample/TreeViewSample/TreeViewSample/ANode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TreeViewSample
+{
+    public class ANode
+    {
+        public ANode(string text)
+        {
+            Text = text;
+        }
+        public string Text { get; set; }
+
+        public override string ToString() => Text;
+
+        public Lazy<IEnumerable<ANode>> ChildNodes => 
+            new Lazy<IEnumerable<ANode>>(() =>
+                new List<ANode>(
+                    Enumerable.Range(0, 10).Select(x => new ANode($"{Text}.{x}")).ToList()));
+    }
+}
---
+++ TreeViewSample/TreeViewSample/TreeViewSample/NodeFactory.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TreeViewSample
+{
+    public class NodeFactory
+    {
+        public IEnumerable<ANode> GetNodes()
+        {
+            return Enumerable.Range(0, 10).Select(x => new ANode($"{x}")).ToList();
+        }
+    }
+}
